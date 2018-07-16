b363e46c8757abf7985d826f826e01b00ccc8ca0
john kenneth furog <furogjohnkenneth@gmail.com>
2018-07-16 11:21:41 +0800

    Add project files.

---
+++ POC_FlexLayout/POC_FlexLayout/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace POC_FlexLayout.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        int _count = 1;
+
+        public ObservableCollection<string> Sources { get; }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+            Sources = new ObservableCollection<string>();
+
+        }
+
+        private DelegateCommand _addCommand;
+        public DelegateCommand AddCommand =>
+            _addCommand ?? (_addCommand = new DelegateCommand(ExecuteAddCommand));
+
+        void ExecuteAddCommand()
+        {
+            Sources.Add("number "+_count);
+            _count++;
+        }
+
+        private DelegateCommand<string> _removeCommand;
+        public DelegateCommand<string> RemoveCommand =>
+            _removeCommand ?? (_removeCommand = new DelegateCommand<string>(ExecuteRemoveCommand));
+
+        void ExecuteRemoveCommand(string parameter)
+        {
+            Sources.Remove(parameter);
+        }
+
+    }
+}
